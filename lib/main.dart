63a94c70bec6f78cc9e4f6252f92a4ba918a101e
Rafael Gioffi <rafaelgioffi@gmail.com>
2025-07-13 12:42:41 -0300

    Buildando a tela principal

--- lib/main.dart
+++ lib/main.dart
@@ -22,24 +22,15 @@ class BomHamburguerApp extends StatelessWidget {
 }
 
 class MenuScreen extends StatefulWidget {
-  const MenuScreen({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MenuScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MenuScreen> createState() => _MenuScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+enum itemtype { sandwich, fries, drink }
+
+class MenuItem {
   int _counter = 0;
 
   void _incrementCounter() {
